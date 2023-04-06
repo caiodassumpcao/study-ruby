653cbb5b2442a365ca7dfd3629739892386cf0e9
caiodassumpcao <caio.rodri.d@gmail.com>
2023-04-06 06:47:57 -0300

    Att project e incrementation project hello_world

--- hello_world.rb
+++ hello_world.rb
@@ -3,7 +3,23 @@
 
 nome = 'Caio'
 
-mensagem = "Bem vindo #{nome}"
+mensagem1 = "Bem vindo #{nome}"
 
-puts mensagem
+mensagem2 = <<~MENSAGEM 
+Olá #{nome}, bem vindo ao meu primeiro teste de ruby!
+MENSAGEM
 
+mensagem3 = %Q(Bienvenido #{nome})
+
+puts mensagem1 
+puts mensagem2
+puts mensagem3  
+#listagem de como definir strings em ruby
+
+#nome = "Caio"
+#nome = 'Caio'   
+#nome = %q(Caio)
+
+puts "O tamanho da minha mensagem 1 é: #{mensagem1.length()}"
+puts "O tamanho da minha mensagem 2 é: #{mensagem2.length()}"
+puts "O tamanho da minha mensagem 3 é: #{mensagem3.length()}"
