02add01a2b68570dc810a012e31b4d086bc76475
caiodassumpcao <caio.rodri.d@gmail.com>
2023-04-07 18:26:23 -0300

    Chavealeatorai: criar senha aleatoria. treinandoruby: Linear search (algoritmo)

---
+++ parte2/treinandoruby.rb
@@ -0,0 +1,44 @@
+#Arrey 
+# tipo 1: lista1 = [1, 2, "3"]
+# tipo 2: lista2 = Arrey.new
+
+#Hashes - Estrutura de chave-valor
+# hash { chave: valor, chave: valor}
+
+# tipo 1: hash = {}
+# tipo 2: hash = Hash.new 
+
+# h = { nome: "Caio", idade: "24"} 
+
+#como acessar um valor do hash?
+#h[:nome] 
+
+#como alterar elemento?
+#{:nome} = "Caio Dassumpção" 
+
+# hash.keys - retorna um array com as chaves
+# hash.values - retorna um array com os valores
+# hash.empty - verifica se o hash está vazio
+
+puts "Digite um número:"
+numero = gets.chomp.to_i
+
+array = [3, 6, 9, 12, 15, 18]
+
+encontrado = false
+indice = -1
+
+array.each_with_index do |elemento, i|
+  if elemento == numero
+    encontrado = true
+    indice = i
+    break
+  end
+end
+
+if encontrado
+  puts "O número #{numero} foi encontrado no índice #{indice}."
+else
+  puts "O número #{numero} não foi encontrado no array."
+end
+
