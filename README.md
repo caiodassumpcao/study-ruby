a38956bd0bbf04c4c50234422a6d156aeea1f5a2
caiodassumpcao <caio.rodri.d@gmail.com>
2023-04-14 07:22:28 -0300

    Inicio do reajuste do README e começo do codigo maximizearray

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # study-ruby
-repositorio para alocar projetos e estudos de ruby
+repositorio para alocar projetos e estudos de ruby:
+ 
+Podem ignorar a "pastateste" e os exercícios estão na "parte2". Alguns comentados e outros não KKK.
--- parte2/maximizearray.rb
+++ parte2/maximizearray.rb
@@ -1,10 +1,34 @@
-def min_sum(array, n)
-  valo_minimo = array.min
-  return valo_minimo * (n-1)
-end
- 
+#Entrada: arr[] = {-2, 0, 5, -1, 2}, K = 4
+#Saída: 10
+#Explicação:
+#1. Substitua (-2) por -(-2), a matriz se torna {2, 0, 5 , -1, 2}
+#2. Substitua (-1) por -(-1), a matriz torna-se {2, 0, 5, 1, 2}
+#3. Substitua (0) por -(0), a matriz torna-se {2, 0, 5, 1, 2}
+#4. Substitua (0) por -(0), a matriz se torna {2, 0, 5, 1, 2}
 
-array = [3, 6, 2, 8, 7, 5]
-n = array.length
+#exercicio prático: arr[] = {9, 8, 8, 5}, k = 3
 
-puts min_sum(array, n)
+#primeiro vamos ordenar o arr
+
+def max_soma (array, k)
+    array_ord = array.sort!
+    x = 0 #variavel para identificar se tem numero negativo ou 0 
+
+    for i in (0..k) 
+        if array[i] < 0
+            array[i] = -array[i]
+        elsif array[i] == 0 
+            numero_zero += 1
+        else    
+            x += 1
+        end
+    end
+
+    until x = 0 
+        do 
+
+
+
+
+
+
