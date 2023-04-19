b1a21902de6e27376426919598d2adaea35a371c
caiodassumpcao <caio.rodri.d@gmail.com>
2023-04-19 19:19:03 -0300

    docs find_future: code has a bug

--- problems/find_future.rb
+++ problems/find_future.rb
@@ -6,4 +6,41 @@
 
 require 'date'
 
-array = array.map {|string| Date.strip(string, "%d/%m/%Y")}
+array = ["22/4/1233", "1/3/633", "23/5/56645", "4/12/233"]
+q = ["12/3/2", "23/3/4345", "1/3/633"]
+
+def find_date(array)
+    inicio = 0 
+    tamanho = array.length
+    fim = tamanho - 1
+
+    date_array = array.map { |str| Date.strptime(str, "%d/%m/%Y") } #transformar os objetos do array em data
+    date_q = q.map { |str| Date.strptime(str, "%d/%m/%Y") }
+
+    sorted_date_array = date_array.sort
+
+    puts sorted_date_array
+    
+    while inicio <= fim
+        meio = (inicio + fim) / 2
+
+        if sorted_date_array[meio] == date_q 
+            return meio 
+        elsif sorted_date_array [meio] < date_q
+            inicio = meio + 1
+        else 
+            fim = meio - 1
+        end
+    end
+    return -1
+
+end 
+
+indice = find_date(array) + 1
+
+if indice == -1
+  puts "A data não foi encontrada"
+else
+  puts "A data foi encontrada no índice #{indice} #{q}"
+end
+
